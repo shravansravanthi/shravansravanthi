34d46c82faae83fcc89250a54f5b365af3a02877
unknown <ShravantekkaReddy>
2019-07-10 22:46:20 -0400

    Mychanges

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "react-toastify/dist/ReactToastify.css";
+
+import HistoricalChart from "./components/HistoricalChart";
+
+const App = props => <HistoricalChart />;
+
+export default App;
